6fda7b38e7e07f46b4e154a18eb8f610ea51837b
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-03-04 14:04:09 +0700

    Header & Initial Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/register"
+              element={<Register />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
